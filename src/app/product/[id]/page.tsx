97a0352fc838df3a291f982d0ba09136dac0dc0c
Giovanni <giovanniminoru@gmail.com>
2025-04-13 20:28:10 -0300

    corrigindo bugs

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -4,10 +4,10 @@ import { Metadata } from "next"
 import Image from "next/image"
 
 interface ProductPageProps {
-    params: {
-      id: string
-    }
-  }
+    params: Promise<{
+        id: string
+    }>
+}
 
 interface ProductProps {
     id: string
@@ -19,36 +19,45 @@ interface ProductProps {
 }
 
 export async function generateMetadata({ params }: ProductPageProps): Promise<Metadata> {
-    const product: ProductProps = await getProduct(params.id)
-  
+    const { id } = await params 
+    const product: ProductProps = await getProduct(id)
+
     return {
-      title: `${product.name} | Ignite Shop`,
-      description: product.description ?? "Not has description.",
+        title: `${product.name} | Ignite Shop`,
+        description: product.description ?? "No description available",
     }
-  }
+}
+
 
 export default async function Product({ params }: ProductPageProps) {
-    const product: ProductProps = await getProduct(params.id)
+    const { id } = await params 
+    const product: ProductProps = await getProduct(id)
 
     return (
         <main className="w-full flex flex-col items-center justify-between m-auto lg:max-w-[1180px] lg:h-[656px] lg:flex-row lg:justify-between">
+            
             <div className="h-full w-full max-w-[280px] flex items-center justify-center bg-[linear-gradient(180deg,_#1EA483_0%,_#7465D4_100%)] rounded-[8px] mb-10 md:max-w-[576px] lg:mb-0">
                 <Image width={520} height={480} src={product.imageUrl} alt={product.name} />
             </div>
 
+            
             <div className="w-full h-full max-w-[280px] flex flex-col items-center justify-between md:max-w-[520px]">
                 <div className="flex flex-col justify-center items-center mb-12 lg:mb-0 lg:items-start">
                     <h2 className="text-gray-200 text-2xl font-bold mb-4 md:text-[32px]">{product.name}</h2>
 
-                    <strong className="text-green-100 text-2xl font-normal mb-10 md:text-[32px]">{product.price.toLocaleString("pt-BR", { 
-                        style: "currency", 
-                        currency: "BRL", 
+                    <strong className="text-green-100 text-2xl font-normal mb-10 md:text-[32px]">
+                        {product.price.toLocaleString("pt-BR", { 
+                            style: "currency", 
+                            currency: "BRL", 
                         })}
                     </strong>
 
-                    <p className="text-gray-200 text-sm font-normal text-center md:text-[18px] lg:text-left">{product.description ?? "Not has description"}</p>
+                    <p className="text-gray-200 text-sm font-normal text-center md:text-[18px] lg:text-left">
+                        {product.description ?? "No description available"}
+                    </p>
                 </div>
 
+                
                 <PurchaseButton priceId={product.defaultPriceId} />
             </div>
         </main>
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -9,55 +9,88 @@ interface OrderData {
     product_image: string
 }
 
-export async function generateMetadata({ searchParams }: { searchParams: { session_id?: string } }): Promise<Metadata> {
-    const sessionId = searchParams.session_id
-  
-    if (!sessionId) {
-      return {
-        title: "Purchase Not Found",
-      }
-    }
-  
-    const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/checkout/session?session_id=${sessionId}`)
-    if (!response.ok) {
-      return {
-        title: "Purchase Not Found",
-      }
-    }
-  
-    const orderData: OrderData = await response.json()
-  
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Promise<{ session_id?: string }>
+}): Promise<Metadata> {
+  const params = await searchParams
+  const sessionId = params.session_id
+
+  if (!sessionId) {
     return {
-      title: `Success: ${orderData.product_name} | Ignite Shop`,
-      description: `Thank you for your purchase, ${orderData.customer_name.split(" ")[0]}!`,
+      title: "Purchase Not Found",
     }
   }
 
-export default async function SuccessPage({ searchParams }: { searchParams: { session_id?: string } }) {
-    const sessionId = searchParams.session_id
-
-    if (!sessionId) {
-        return notFound()
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/checkout/session?session_id=${sessionId}`
+  )
+  if (!response.ok) {
+    return {
+      title: "Purchase Not Found",
     }
+  }
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/checkout/session?session_id=${sessionId}`)
-    if (!response.ok) {
-        return notFound()
-    }
+  const orderData: OrderData = await response.json()
 
-    const orderData: OrderData = await response.json()
+  return {
+    title: `Success: ${orderData.product_name} | Ignite Shop`,
+    description: `Thank you for your purchase, ${orderData.customer_name.split(" ")[0]}!`,
+  }
+}
 
-    return (
-        <main className="mt-[-64px] flex flex-col items-center justify-center h-[656px] w-full max-w-[590px] m-auto lg:mt-0">
-           <h3 className="text-gray-100 text-2xl font-bold mb-16 md:text-[32px]">Purchase successful!</h3>
+export default async function SuccessPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ session_id?: string }>
+}) {
+  const params = await searchParams
+  const sessionId = params.session_id
 
-           <div className="w-[127px] h-[145px] flex items-center justify-center bg-[linear-gradient(180deg,_#1EA483_0%,_#7465D4_100%)] rounded-[8px] mb-8">
-                <Image src={orderData.product_image} alt={orderData.product_name} width={114} height={106} />
-           </div>
+  if (!sessionId) {
+    return notFound()
+  }
 
-           <p className="text-gray-200 font-normal text-[16px] mb-10 px-6 text-center md:mb-20 md:text-2xl">Congratulations <strong className="text-gray-200 font-bold text-[16px] md:text-2xl">{orderData.customer_name.split(" ")[0]}</strong>, your <strong className="text-gray-200 font-bold text-[16px] md:text-2xl">{orderData.product_name}</strong> is already on the way to your house.</p>
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/checkout/session?session_id=${sessionId}`
+  )
+  if (!response.ok) {
+    return notFound()
+  }
 
-           <Link href={'/'} className="text-green-300 text-[16px] font-bold md:text-xl">Return to catalog</Link>
-        </main>
-    )
+  const orderData: OrderData = await response.json()
+
+  return (
+    <main className="mt-[-64px] flex flex-col items-center justify-center h-[656px] w-full max-w-[590px] m-auto lg:mt-0">
+      <h3 className="text-gray-100 text-2xl font-bold mb-16 md:text-[32px]">
+        Purchase successful!
+      </h3>
+
+      <div className="w-[127px] h-[145px] flex items-center justify-center bg-[linear-gradient(180deg,_#1EA483_0%,_#7465D4_100%)] rounded-[8px] mb-8">
+        <Image
+          src={orderData.product_image}
+          alt={orderData.product_name}
+          width={114}
+          height={106}
+        />
+      </div>
+
+      <p className="text-gray-200 font-normal text-[16px] mb-10 px-6 text-center md:mb-20 md:text-2xl">
+        Congratulations{" "}
+        <strong className="text-gray-200 font-bold text-[16px] md:text-2xl">
+          {orderData.customer_name.split(" ")[0]}
+        </strong>
+        , your{" "}
+        <strong className="text-gray-200 font-bold text-[16px] md:text-2xl">
+          {orderData.product_name}
+        </strong>{" "}
+        is already on the way to your house.
+      </p>
+
+      <Link href={"/"} className="text-green-300 text-[16px] font-bold md:text-xl">
+        Return to catalog
+      </Link>
+    </main>
+  )
 }
